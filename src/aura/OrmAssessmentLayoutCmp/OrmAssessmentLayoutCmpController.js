7b26da241ab6df376371074c5e58e21777c2a872
Dibocor <dibocorng@gmail.com>
2018-07-27 20:15:04 +0200

    ajout des custom label dans le controleur js de assessmentLayoutCmp

--- src/aura/OrmAssessmentLayoutCmp/OrmAssessmentLayoutCmpController.js
+++ src/aura/OrmAssessmentLayoutCmp/OrmAssessmentLayoutCmpController.js
@@ -13,7 +13,7 @@
         var showRiskTreatment = component.get("v.showRiskTreatment");
         var showData = component.get("v.showData");
         if(showContext == true){
-            var assessmentName = component.find("Name").get("v.value");
+           /* var assessmentName = component.find("Name").get("v.value");
             //console.log('assessmentName:::'+assessmentName);
             if(assessmentName =='' || assessmentName == null){
                 component.set("v.setMessage",'error');           
@@ -28,13 +28,13 @@
                 
             }
             else
-            { 
+            { */
                 component.set("v.showContext", false);
                 component.set("v.showContext2", true);
                 component.set("v.showError", false);
                 helper.activeContext2(component, event);
                 
-            }
+           // }
             
         }  
         if(showContext2 == true){
@@ -284,24 +284,24 @@
     },
     onTypeAssessmentChange : function(component,event,helper){ 
         if(event.getSource().get("v.value").trim() != ''){ 
-            component.find("typeAssessment").set("v.value", event.getSource().get("v.value"));
-            component.set("v.displaySaveCancelBtn",true);
-            
+            component.find("typeAssessment").set("v.value", event.getSource().get("v.value"));            
         }
+        var field = $A.get("$Label.c.orm_type_assessment");
+        var description = $A.get("$Label.c.orm_description_type_assessment");
+        helper.sendValuesToFieldDescription(component, event, helper, field, description);
+        
     },
+    
     onTitleChange : function(component,event,helper){ 
         if(event.getSource().get("v.value").trim() != ''){ 
             component.set("v.displaySaveCancelBtn",true);
         }
     },
-    sendValueToFD : function(component,event,helper){ 
-            component.set("v.closeFieldDescription",false);
-            var evt = $A.get("e.c:OrmSendValuesToFieldDescriptionEvt");
-            evt.setParams({
-				"nomField" : "Title",
-				"descriptionField" : "This field defines the title of assessment"
-			});
-		    evt.fire();
+    sendTitleToFD : function(component,event,helper){ 
+    	
+        var field = $A.get("$Label.c.orm_title_assessment");
+        var description = $A.get("$Label.c.orm_description_assessment");
+        helper.sendValuesToFieldDescription(component, event, helper, field, description);
     },
     
     onObjectifChange : function(component,event,helper){ 

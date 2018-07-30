e5eae3567681a3335a4161cbbc054b55627436a3
ngomsalimata <salifadou@gmail.com>
2018-07-30 17:54:12 +0000

    rename component activity

--- src/aura/OrmActivityList/OrmActivityListHelper.js
+++ src/aura/OrmActivityList/OrmActivityListHelper.js
@@ -1,33 +1,16 @@
 ({
-	// fetch picklist values dynamic from apex controller 
-    fetchPickListVal: function(component, fieldName, picklistOptsAttributeName) {
-        var action = component.get("c.getSelectOptions");
-        action.setParams({
-            "objObject": component.get("v.objInfoForPicklistValues"),
-            "fld": fieldName
-        });
-        var opts = [];
-        action.setCallback(this, function(response) {
-            if (response.getState() == "SUCCESS") {
-                var allValues = response.getReturnValue();
- 
-                if (allValues != undefined && allValues.length > 0) {
-                    opts.push({
-                        class: "optionClass",
-                        label: "--- None ---",
-                        value: ""
-                    });
-                }
-                for (var i = 0; i < allValues.length; i++) {
-                    opts.push({
-                        class: "optionClass",
-                        label: allValues[i],
-                        value: allValues[i]
-                    });
-                }
-                component.set("v." + picklistOptsAttributeName, opts);
-            }
-        });
-        $A.enqueueAction(action);
+
+    requiredValidation : function(component,event) {
+        // get all activity.. 	
+        var allRecords = component.get("v.ActivityList");
+        var isValid = true;
+        // play a for loop on all activity list and check that activty name is not null,   
+        for(var i = 0; i < allRecords.length;i++){
+            if(allRecords[i].Name == null || allRecords[i].Name.trim() == ''){
+                alert('Complete this field : Row No ' + (i+1) + ' Name is null' );
+                isValid = false;
+            }  
+        }
+        return isValid;
     },
 })

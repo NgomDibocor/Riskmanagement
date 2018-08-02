3bb680fda1d118c75a8558185b896257e384960f
ngomsalimata <salifadou@gmail.com>
2018-08-02 18:35:33 +0000

    new workshop

--- src/aura/OrmWorkShopNewCmp/OrmWorkShopNewCmpController.js
+++ src/aura/OrmWorkShopNewCmp/OrmWorkShopNewCmpController.js
@@ -24,16 +24,10 @@
          newItem.orm_assessment__c=  "a051H00000aQzDNQA0";
             newItem.Name=name.get('v.value');
             newItem.StartDate=datestart.get('v.value');
-<<<<<<< HEAD
            newItem.Contract_End_Date__c=dateend.get('v.value');
            newItem.CompanySignedDate=dateinvitation.get('v.value');
            newItem.Description=message.get('v.value');
            newItem.AccountId="0011H00001RnBahQAF";
-=======
-           newItem.EndDate=dateend.get('v.value');
-           newItem.CompanySignedDate=dateinvitation.get('v.value');
-           newItem.Description=message.get('v.value');
->>>>>>> 2bb701fc4ae710edc8c11654a284149230663cae
            alert(JSON.stringify(newItem));
             var action = component.get('c.addWorkShop');
             action.setParams({
@@ -53,16 +47,10 @@
                                 'sobjectType' : 'Contract',
                         'Name': '',
                         'StartDate':'',
-<<<<<<< HEAD
                         'Contract_End_Date__c':'', 
                         'CompanySignedDate':'',
                         'Assessment__c':'',
                         'AccountId':'',                               
-=======
-                        'EndDate':'', 
-                        'CompanySignedDate':'',
-                        'orm_assessment__c':'',                               
->>>>>>> 2bb701fc4ae710edc8c11654a284149230663cae
 						'Description':''
                             });
                         }else

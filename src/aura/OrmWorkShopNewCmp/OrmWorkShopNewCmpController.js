b57dcf770e87f29bf06163c3b9694e1e47fcb04c
ngomsalimata <salifadou@gmail.com>
2018-08-02 14:35:55 +0000

    add new workshop

---
+++ src/aura/OrmWorkShopNewCmp/OrmWorkShopNewCmpController.js
@@ -0,0 +1,69 @@
+({
+
+    createWorkshop: function(component, event, helper) {
+ 
+        var name = component.find("idtitre");
+        var datestart = component.find('datestart');
+        var dateend = component.find('dateend');
+        var message =  component.find('message');
+        var dateinvitation =  component.find('dateinvitation');
+       
+        var isItemValid = true;
+        if ($A.util.isEmpty(name.get('v.value')) || $A.util.isEmpty(datestart.get('v.value')) || $A.util.isEmpty(dateend.get('v.value'))) {
+            isItemValid = false;
+            
+        } 
+       
+        if (isItemValid) {
+      
+        // new item workshop
+       
+            var newItem = component.get("v.item");
+           
+           //newItem.orm_assessment__c = component.get('v.assessmentData').Id;
+         newItem.orm_assessment__c=  "a051H00000aQzDNQA0";
+            newItem.Name=name.get('v.value');
+            newItem.StartDate=datestart.get('v.value');
+           newItem.Contract_End_Date__c=dateend.get('v.value');
+           newItem.CompanySignedDate=dateinvitation.get('v.value');
+           newItem.Description=message.get('v.value');
+           newItem.AccountId="0011H00001RnBahQAF";
+           alert(JSON.stringify(newItem));
+            var action = component.get('c.addWorkShop');
+            action.setParams({
+                "item": newItem
+            });
+               action
+                .setCallback(
+                    this,
+                    function(response) {
+                        var state = response.getState();
+                        if (state == "SUCCESS") {
+                        alert('success');
+                            var evt = $A.get("e.c:OrmNewWorkShopEvt");
+                            evt.fire();
+
+                            component.set("v.item", {
+                                'sobjectType' : 'Contract',
+                        'Name': '',
+                        'StartDate':'',
+                        'Contract_End_Date__c':'', 
+                        'CompanySignedDate':'',
+                        'Assessment__c':'',
+                        'AccountId':'',                               
+						'Description':''
+                            });
+                        }else
+                        {
+                             alert('work');
+                        }
+                    });
+            $A.enqueueAction(action);
+           
+
+        } else {
+          //  alert("ajout échouée");
+        }
+       
+    }
+})

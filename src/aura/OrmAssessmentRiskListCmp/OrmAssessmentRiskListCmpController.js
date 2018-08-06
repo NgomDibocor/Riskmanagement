548702dfc1cead8a0cd793e238f333e4d43ee03b
davidmignane <diopdavid94@gmail.com>
2018-08-06 12:11:40 +0100

    list risk

--- src/aura/OrmAssessmentRiskListCmp/OrmAssessmentRiskListCmpController.js
+++ src/aura/OrmAssessmentRiskListCmp/OrmAssessmentRiskListCmpController.js
@@ -4,11 +4,21 @@
 	},
     doInit : function(component, event, helper)
     {
-        helper.refreshList(component, event);
-        var action = component.get('c.findAll');
+       // helper.refreshList(component, event);
+        var action = component.get('c.getRisks');
         action.setCallback(this, function(response){
-            if(response.getState() == 'SUCCESS'){
+           /* if(response.getState() == 'SUCCESS'){
                 component.set('v.assessmentRisks', response.getReturnValue());
+            }*/
+            var state = response.getState();
+             if(state === 'SUCCESS'){
+                var custs = [];
+                var conts = response.getReturnValue();
+                for(var idAss in conts){
+                    custs.push({value:conts[idAss]});
+                }
+                component.set("v.assessmentRisks", custs);
+                
             }
             else
             {

32b27f1824a182d0b277430a573dcab21377f66b
Dibocor <dibocorng@gmail.com>
2018-08-08 21:25:06 +0200

    pagination

--- src/aura/OrmAssessmentListCmp/OrmAssessmentListCmpController.js
+++ src/aura/OrmAssessmentListCmp/OrmAssessmentListCmpController.js
@@ -9,18 +9,17 @@
                 for(var idAss in conts){
                     custs.push({value:conts[idAss]});
                 }
-                component.set("v.assessments", custs);
+                component.set("v.items", custs);
                 
                 // start
                     var pageSize = component.get("v.pageSize");
 				                
 	                // get size of all the records and then hold into an attribute "totalRecords"
-	                component.set("v.totalRecords", component.get("v.assessments").length);
+	                component.set("v.totalRecords", component.get("v.items").length);
 	                // set star as 0
 	                component.set("v.startPage",0);
 	                
-	                var totalRecords = component
-					.get("v.assessments").length;
+	                var totalRecords = component.get("v.items").length;
 				    //var div = Math.trunc(totalRecords / pageSize);
 	                if(totalRecords === pageSize){
 	                  component.set("v.hideNext", true);
@@ -33,11 +32,11 @@
 	                }
 	                var PaginationList = [];
 	                for(var i=0; i< pageSize; i++){
-	                    if(component.get("v.assessments").length> i)
-	                        PaginationList.push(response.getReturnValue()[i]);    
+	                    if(component.get("v.items").length> i)
+	                        PaginationList.push(component.get("v.items")[i]);    
 	                }
 	                component.set('v.PaginationList', PaginationList);
-	                alert(JSON.stringify(component.get("v.PaginationList")))
+	                //alert(JSON.stringify(component.get("v.PaginationList").lenght))
                 //end
                 
             } else {
@@ -71,4 +70,9 @@
 		});
 		$A.enqueueAction(action);
 	},
+	
+	filter : function(component, event, helper) {
+	
+	},
+	
 })

b93d9dbfcc3992fdf3203f34f0b219534956ec0e
layepythagore <layepythagore.91@gmail.com>
2018-08-08 12:45:42 +0000

    internationalize phas*Cmp

--- src/aura/OrmPhaseEditRow/OrmPhaseEditRowHelper.js
+++ src/aura/OrmPhaseEditRow/OrmPhaseEditRowHelper.js
@@ -1,17 +1,15 @@
 ({
    // fetch picklist values dynamic from apex controller 
     fetchPickListVal: function(component, fieldName, picklistOptsAttributeName) {
-    	console.log("laye");
         var action = component.get("c.getSelectOptions");
         action.setParams({
             "objObject": component.get("v.objInfo"),
-            "fld": fieldName
+            "fld": 'orm_phase__c'
         });
         var opts = [];
         action.setCallback(this, function(response) {
             if (response.getState() == "SUCCESS") {
                 var allValues = response.getReturnValue();
-                console.log("allValues " + allValues);
  
                 if (allValues != undefined && allValues.length > 0) {
                     opts.push({
@@ -27,7 +25,9 @@
                         value: allValues[i]
                     });
                 }
-                component.set("v." + picklistOptsAttributeName, opts);
+                component.set("v.allPhases", opts);
+                console.log("component.get('v.allPhases') " + 
+                		JSON.stringify(component.get("v.allPhases")));
             }
         });
         $A.enqueueAction(action);

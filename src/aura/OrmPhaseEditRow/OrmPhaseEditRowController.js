518a2ea910d3b1c092c3353a70d512018c01e72b
layepythagore <layepythagore.91@gmail.com>
2018-08-10 16:54:45 +0000

    add delete in list phase

--- src/aura/OrmPhaseEditRow/OrmPhaseEditRowController.js
+++ src/aura/OrmPhaseEditRow/OrmPhaseEditRowController.js
@@ -30,4 +30,66 @@
             component.set("v.showSaveCancelBtn",true);
         }
     },
+    
+    //For Delete selected records 
+    deleteSelected: function(component, event, helper) {
+    	// create var for store record id's for selected checkboxes  
+    	var delId = [];
+    	// get all checkboxes 
+    	var getAllId = component.find("boxPack");
+    	// If the local ID is unique[in single record case], find() returns the component. not array
+    	if(! Array.isArray(getAllId)){
+    		if (getAllId.get("v.value") == true) {
+    			delId.push(getAllId.get("v.text"));
+    		}
+    	} else {
+    		// play a for loop and check every checkbox values 
+    		// if value is checked(true) then add those Id (store in Text attribute on checkbox) in delId var.
+    		for (var i = 0; i < getAllId.length; i++) {
+    			if (getAllId[i].get("v.value") == true) {
+    				delId.push(getAllId[i].get("v.text"));
+    			}
+    		}
+    	}   
+    	// call the helper function and pass all selected record id's.    
+    	helper.deleteSelectedHelper(component, event, delId);        
+    },
+    
+    selectAll : function(component, event, helper) {
+    	
+    	var selectedHeaderCheck = event.getParam("selectAllCheckbox");
+    	// get all checkbox on table with "boxPack" aura id (all iterate value have same Id)
+    	// return the List of all checkboxs element 
+    	var getAllId = component.find("boxPack");
+    	// If the local ID is unique[in single record case], find() returns the component. not array   
+    	if(! Array.isArray(getAllId)){
+    		if(selectedHeaderCheck == true){ 
+    			component.find("boxPack").set("v.value", true);
+    			//component.set("v.selectedCount", 1);
+    		} else {
+    			component.find("boxPack").set("v.value", false);
+    			//component.set("v.selectedCount", 0);
+    		}
+    	} else {
+    		// check if select all (header checkbox) is true then true all checkboxes on table in a for loop  
+    		// and set the all selected checkbox length in selectedCount attribute.
+    		// if value is false then make all checkboxes false in else part with play for loop 
+    		// and select count as 0 
+    		if (selectedHeaderCheck == true) {
+    			 for (var i = 0; i < getAllId.length; i++) {
+    				component.find("boxPack")[i].set("v.value", true);
+    				//component.set("v.selectedCount", getAllId.length);
+    			 }
+    		} else {
+    			for (var i = 0; i < getAllId.length; i++) {
+    				component.find("boxPack")[i].set("v.value", false);
+    				component.set("v.selectedCount", 0);
+    			}
+    		} 
+    	 }    	
+    	
+    	
+    	
+    }
+    
 })

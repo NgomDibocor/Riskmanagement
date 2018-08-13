27d32dcfd2606b83bd7de992f4a3d0ff5504b353
davidmignane <diopdavid94@gmail.com>
2018-08-13 17:01:47 +0100

    test

--- src/aura/OrmAssessmentRiskConfigCmp/OrmAssessmentRiskConfigCmpController.js
+++ src/aura/OrmAssessmentRiskConfigCmp/OrmAssessmentRiskConfigCmpController.js
@@ -1,5 +1,9 @@
 ({
-	myAction : function(component, event, helper) {
-		
-	}
+	/*
+     * CreatedBy @David Diop
+     *
+     */
+    doInit: function(component, event, helper) {
+        
+      },
 })

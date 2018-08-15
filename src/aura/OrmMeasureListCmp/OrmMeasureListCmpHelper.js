5a74c916c500ff0f08c0d0c0aa234d3559122037
Dibocor <dibocorng@gmail.com>
2018-08-15 22:19:55 +0200

    implementation de l'affichage de la liste des mesures

--- src/aura/OrmMeasureListCmp/OrmMeasureListCmpHelper.js
+++ src/aura/OrmMeasureListCmp/OrmMeasureListCmpHelper.js
@@ -1,5 +1,14 @@
 ({
-	helperMethod : function() {
+	getAllMeasuresByAssessmentRisk : function(component, event) {	
 		
+        var action = component.get("c.getAllMeasuresByAssessmentRisk");
+        action.setParam('idAssessmentRisk', component.get("v.assessmentRiskId"));
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {   
+            	component.set("v.measures ", response.getReturnValue());
+            }
+        });
+        $A.enqueueAction(action);
 	}
 })

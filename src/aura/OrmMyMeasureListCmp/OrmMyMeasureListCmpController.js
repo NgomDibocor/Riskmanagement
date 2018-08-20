76190f5f9c6ad7bffbb0552504af8a77bb709c99
David Diop <diopdavid94@gmail.com>
2018-08-20 16:47:33 +0100

    test

--- src/aura/OrmMyMeasureListCmp/OrmMyMeasureListCmpController.js
+++ src/aura/OrmMyMeasureListCmp/OrmMyMeasureListCmpController.js
@@ -7,5 +7,21 @@ showAssessmentRisk : function(component, event, helper)
     {
 	     var measureId = event.target.id;
          var idAssessment = document.getElementById( measureId ).getElementsByTagName( 'span' ).item(0).id;
+         var action = component.get('c.getAssessment');
+	     action.setParams({"idAss": idAssessment });
+	     action.setCallback(this, function(response){
+	          var state = response.getState();
+	          if(state === 'SUCCESS'){
+		         var evt = $A.get("e.c:OrmShowMeasureInfoEvt");
+			     evt.setParams({
+			       "assessmentObject" : response.getReturnValue(),
+			       "idMeasure" : measureId
+			     });
+			     evt.fire();        
+		     } else {
+		        alert("l'élément n'a pas été chargé");
+		     }
+	     });
+	     $A.enqueueAction(action);
 	},
 })

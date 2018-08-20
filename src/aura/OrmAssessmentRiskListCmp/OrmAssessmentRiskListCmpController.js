5b0e1dd09c7620b68546bb484e9ff4e417723c26
Dibocor <dibocorng@gmail.com>
2018-08-20 18:55:26 +0200

    correction filter

--- src/aura/OrmAssessmentRiskListCmp/OrmAssessmentRiskListCmpController.js
+++ src/aura/OrmAssessmentRiskListCmp/OrmAssessmentRiskListCmpController.js
@@ -1,6 +1,6 @@
 ({
     doInit : function(component, event, helper)
-    {
+    {  
        helper.refreshList(component, event);
     },
     showAssessmentRisk : function(component, event, helper) {

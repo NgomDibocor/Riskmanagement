9bbb10788d947fc07dd8aeb0baa23bb8fee18318
David Diop <diopdavid94@gmail.com>
2018-08-20 12:02:37 +0100

    test

--- src/aura/OrmMyMeasureListCmp/OrmMyMeasureListCmpController.js
+++ src/aura/OrmMyMeasureListCmp/OrmMyMeasureListCmpController.js
@@ -2,5 +2,10 @@
 doInit : function(component, event, helper)
     {
        helper.refreshList(component, event);
-    },}
+    },
+showAssessmentRisk : function(component, event, helper) 
+    {
+         var evtSpinner = $A.get("e.c:OrmShowSpinnerEvt");
+	     evtSpinner.fire(); 
+	},
 })

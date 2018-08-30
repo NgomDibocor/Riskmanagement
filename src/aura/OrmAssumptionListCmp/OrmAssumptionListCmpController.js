caf62c6e56870877fedee1af1db49cb5ea4b4a4b
ngomsalimata <salifadou@gmail.com>
2018-08-30 13:22:08 +0000

    assumption new

---
+++ src/aura/OrmAssumptionListCmp/OrmAssumptionListCmpController.js
@@ -0,0 +1,24 @@
+({
+	    openNewAssumption : function(component, event, helper){
+                
+        var idAssessment = component.get("v.assessmentData").Id;
+        if(idAssessment == null){
+        	//alert("check if you have created the assessment");
+        	var toast = $A.get('e.force:showToast');
+            toast.setParams({
+            	'message' : $A.get("$Label.c.orm_toast_warning"),
+                'type' : 'warning',
+                'mode' : 'dismissible'
+            });
+
+            toast.fire();
+        } else {
+        	var evt = $A.get("e.c:OrmNewAssumptionClickedEvt");
+			evt.setParams({
+			   "Assessmentdata" : component.get("v.assessmentData")
+			});
+			evt.fire();
+        }
+    },
+    
+})
---
+++ src/aura/OrmAssumptionNewCmp/OrmAssumptionNewCmpController.js
@@ -0,0 +1,12 @@
+({
+	/**
+	 * @author: Salimata NGOM
+	 * @date: Creation: 30/08/2018
+	 * @description: method for opening the modal form
+	 *               
+	 */
+	openModalAssumption : function(component, event, helper) {
+		component.set("v.isOpen", true);
+		
+	},
+})

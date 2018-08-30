eb92bf4416c04163d90cafefad03e45be445c9c4
David Diop <diopdavid94@gmail.com>
2018-08-30 10:33:33 +0100

    measure list refresh

---
+++ src/aura/OrmMeasureListCmp/OrmMeasureListCmpController.js
@@ -0,0 +1,103 @@
+({
+	getIdAssessmentRisk : function(component, event, helper) {
+		component.set("v.idAssessmentRisk", event.getParam('idAssessmentRisk'));
+		//console.log('idAssessmentRisk ', event.getParam('idAssessmentRisk'));
+		helper.getAllMeasuresByAssessmentRisk(component, event);
+	},
+	
+	refreshList : function(component, event, helper) {
+		helper.getAllMeasuresByAssessmentRisk(component, event);
+	},
+	
+	save: function(component, event, helper) {
+		// Check required fields(Description) first in helper method which is return true/false
+        if (helper.requiredValidation(component, event)){
+               var action = component.get("c.updateMeasures");
+               action.setParams({
+            	   'measures': component.get("v.measures")
+               });
+                  
+	           action.setCallback(this, function(response) {
+	               var state = response.getState();
+	               if (state === "SUCCESS") {
+	                    var measures = response.getReturnValue();
+	                    // set cause list with return value from server.
+	                    component.set("v.measures", measures);
+	                    // Hide the save and cancel buttons by setting the 'showSaveCancelBtn' false 
+	                    component.set("v.showSaveCancelBtn", false);
+	                    var toast = $A.get('e.force:showToast');
+			            toast.setParams({
+			            	'message' : $A.get('$Label.c.orm_updated'),
+			                'type' : 'success',
+			                'mode' : 'dismissible'
+			            });	
+			            toast.fire();
+			       }
+	           });
+	           $A.enqueueAction(action);
+        } 
+		
+	},
+	
+	cancel : function(component,event,helper) {
+       // on cancel refresh the view (This event is handled by the one.app container. It’s supported in Lightning Experience, the Salesforce app, and Lightning communities. ) 
+       $A.get('e.force:refreshView').fire(); 
+    },
+	
+	filterMeasure : function (component, event, helper){
+    	
+    	var measures = component.get('v.measures');
+    	var data = measures;
+    	var key = component.get('v.key');
+    	var regex;    	
+    	
+    	if ($A.util.isEmpty(key)) {    	
+    		helper.getAllMeasuresByAssessmentRisk(component, event);    		      
+         } else {
+        	key = "^" + key;
+        	try {
+        	 		regex = new RegExp(key, "i");
+        	 		// filter checks each row, constructs new array where function returns true
+        	 		data = data.filter(row => regex.test(row.Name)|| regex.test(row.Description));
+		        } catch (e) {
+		    	   
+		        }
+		   component.set("v.measures", data);
+         }        	
+    },
+    
+    openNewMeasureCmp : function (component, event, helper){
+        var evt = $A.get("e.c:OrmNewMeasureClickedEvt");
+		evt.fire();
+    },
+    
+    sendDescriptionFieldMeasure : function (component, event, helper){
+    
+    },
+    
+    openModalDeleteMeasure : function (component, event, helper){
+    	component.set('v.openModalConfirmDeletion', true);
+    },
+    
+    cancelDeleteMeasure : function (component, event, helper){
+    	component.set('v.openModalConfirmDeletion', false);
+    },
+    
+    confirmDeleteMeasure : function (component, event, helper){
+    	var evt = $A.get('e.c:OrmDeleteMeasuresEvt');
+    	evt.setParams({'idAssessmentRisk': component.get('v.idAssessmentRisk')});
+    	evt.fire();
+    	component.set('v.openModalConfirmDeletion', false);
+    },
+    
+    showButtonDelete  : function (component, event, helper) {
+    	var showButtonDelete = event.getParam('showButtonDelete');
+    	console.log('capture event '+ showButtonDelete);
+    	if(showButtonDelete){
+    		component.set('v.showButtonDelete', true);
+    	} else {
+			component.set('v.showButtonDelete', false);
+		}
+    }
+    
+})
---
+++ src/aura/OrmMeasureListCmp/OrmMeasureListCmpHelper.js
@@ -0,0 +1,29 @@
+({	
+	requiredValidation : function(component,event) {
+        // get all causes.. 	
+        var allRecords = component.get("v.measures");
+        var isValid = true;
+        // play a for loop on all account list and check that account name is not null,   
+        for(var i = 0; i < allRecords.length; i++) {
+            if(allRecords[i].orm_description__c == null || allRecords[i].orm_description__c.trim() == ''){
+                alert('Complete this field : Row No ' + (i+1) + ' Description is null' );
+                isValid = false;
+            }  
+        }
+        return isValid;
+    },
+    
+	getAllMeasuresByAssessmentRisk : function(component, event) {	
+		
+        var action = component.get("c.getAllMeasuresByAssessmentRisk");
+        action.setParam('idAssRisk', component.get("v.idAssessmentRisk"));
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+            	//console.log('measure '+ JSON.stringify(response.getReturnValue()));
+            	component.set("v.measures ", response.getReturnValue());
+            }
+        });
+        $A.enqueueAction(action);
+	}
+})

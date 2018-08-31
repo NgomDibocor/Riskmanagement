0b73c6901f11a0acf803d1b4b72e8ea234bbb533
David Diop <diopdavid94@gmail.com>
2018-08-31 12:32:22 +0100

    measureprogression

---
+++ src/aura/OrmMeasureProgressionEditRowCmp/OrmMeasureProgressionEditRowCmpController.js
@@ -0,0 +1,112 @@
+({
+	doInit : function(component, event, helper) {
+		component.set("v.selectedCount", 0);
+	},
+	inlineEditDateProgression : function(component, event, helper) {
+		// show the name edit field popup 
+        component.set("v.dateProgressionEditMode", true); 
+        // after the 100 millisecond set focus to input field   
+        setTimeout(function(){ 
+            component.find("idDateProgression").focus();
+        }, 100);
+	},
+	 closeDateProgressionBox : function(component, event, helper){ 
+    	// on focus out, close the input section by setting the 'nameEditMode' att. as false   
+        component.set("v.dateProgressionEditMode", false); 
+        // check if change/update Name field is blank, then add error class to column -
+        // by setting the 'showErrorClass' att. as True , else remove error class by setting it False   
+        if(event.getSource().get("v.value").trim() == ''){
+            component.set("v.showErrorClass", true);
+        } else {
+            component.set("v.showErrorClass", false);
+        }
+    },
+    onDateProgressionChange : function(component, event, helper){ 
+    	// if edit field value changed and field not equal to blank,
+        // then show save and cancel button by set attribute to true
+        if(event.getSource().get("v.value").trim() != ''){ 
+            component.set("v.showSaveCancelBtn",true);
+        }
+    },
+    
+   inlineEditPourcentage : function(component, event, helper) {
+		// show the name edit field popup 
+        component.set("v.pourcentageEditMode", true); 
+        // after the 100 millisecond set focus to input field   
+        setTimeout(function(){ 
+            component.find("idPercent").focus();
+        }, 100);
+	},
+	 closePourcentageBox : function(component, event, helper){ 
+    	// on focus out, close the input section by setting the 'nameEditMode' att. as false   
+        component.set("v.pourcentageEditMode", false); 
+        // check if change/update Name field is blank, then add error class to column -
+        // by setting the 'showErrorClass' att. as True , else remove error class by setting it False   
+        if(event.getSource().get("v.value").trim() == ''){
+            component.set("v.showErrorClass", true);
+        } else {
+            component.set("v.showErrorClass", false);
+        }
+    },
+    onPourcentageChange : function(component, event, helper){ 
+    	// if edit field value changed and field not equal to blank,
+        // then show save and cancel button by set attribute to true
+        if(event.getSource().get("v.value").trim() != ''){ 
+            component.set("v.showSaveCancelBtn",true);
+        }
+    },
+    
+     inlineEditpoucentageProgression : function(component, event, helper) {
+		// show the name edit field popup 
+        component.set("v.poucentageProgressionEditMode", true); 
+        // after the 100 millisecond set focus to input field   
+        setTimeout(function(){ 
+            component.find("idPoucentageProgression").focus();
+        }, 100);
+	},
+	 closePoucentageProgressionBox : function(component, event, helper){ 
+    	// on focus out, close the input section by setting the 'nameEditMode' att. as false   
+        component.set("v.poucentageProgressionEditMode", false); 
+        // check if change/update Name field is blank, then add error class to column -
+        // by setting the 'showErrorClass' att. as True , else remove error class by setting it False   
+        if(event.getSource().get("v.value").trim() == ''){
+            component.set("v.showErrorClass", true);
+        } else {
+            component.set("v.showErrorClass", false);
+        }
+    },
+    onPoucentageProgressionChange : function(component, event, helper){ 
+    	// if edit field value changed and field not equal to blank,
+        // then show save and cancel button by set attribute to true
+        if(event.getSource().get("v.value").trim() != ''){ 
+            component.set("v.showSaveCancelBtn",true);
+        }
+    },
+    
+     inlineEditDescription : function(component, event, helper) {
+		// show the name edit field popup 
+        component.set("v.DescriptionEditMode", true); 
+        // after the 100 millisecond set focus to input field   
+        setTimeout(function(){ 
+            component.find("idDescription").focus();
+        }, 100);
+	},
+	closeDescriptionBox : function(component, event, helper){ 
+    	// on focus out, close the input section by setting the 'nameEditMode' att. as false   
+        component.set("v.DescriptionEditMode", false); 
+        // check if change/update Name field is blank, then add error class to column -
+        // by setting the 'showErrorClass' att. as True , else remove error class by setting it False   
+        if(event.getSource().get("v.value").trim() == ''){
+            component.set("v.showErrorClass", true);
+        } else {
+            component.set("v.showErrorClass", false);
+        }
+    },
+    onDescriptionChange : function(component, event, helper){ 
+    	// if edit field value changed and field not equal to blank,
+        // then show save and cancel button by set attribute to true
+        if(event.getSource().get("v.value").trim() != ''){ 
+            component.set("v.showSaveCancelBtn",true);
+        }
+    },
+})

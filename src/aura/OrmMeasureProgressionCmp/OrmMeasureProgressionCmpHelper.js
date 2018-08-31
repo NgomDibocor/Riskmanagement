40b1f2a9219c6a96ab21c0980365d31ae613febd
David Diop <diopdavid94@gmail.com>
2018-08-31 17:25:11 +0000

    measure progression List and measure progression EditRow

--- src/aura/OrmMeasureProgressionCmp/OrmMeasureProgressionCmpHelper.js
+++ src/aura/OrmMeasureProgressionCmp/OrmMeasureProgressionCmpHelper.js
@@ -1,5 +1,25 @@
 ({
-	helperMethod : function() {
+	getAllMeasuresProgressionByMeasure : function(component, event) {	
 		
-	}
+        var action = component.get("c.getAllMeasuresProgressionByMeasure");
+        action.setParam('idMeasure', component.get("v.idMeasure"));
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+            	//console.log('measure '+ JSON.stringify(response.getReturnValue()));
+            	component.set("v.measureProgression ", response.getReturnValue());
+            }else{
+            	alert($A.get('$Label.c.orm_not_found'));
+            }
+        });
+        $A.enqueueAction(action);
+	},
+	requiredValidation : function(component,event) {
+        // get all causes.. 	
+        var allRecords = component.get("v.measureProgression");
+        
+        var isValid = true;
+        
+        return isValid;
+    },
 })
---
+++ src/aura/OrmMeasureProgressionNewCmp/OrmMeasureProgressionNewCmpController.js
@@ -0,0 +1,71 @@
+({
+	openOrmMeasureProgressionNewCmp : function(component, event, helper) {
+		component.set("v.isOpen", true);
+        component.set('v.idMeasure', event.getParam('idMeasure'));
+        
+         var action = component.get('c.getSelectOptions');    
+        action.setParams({'objObject' : component.get("v.measureProgression"), 'fld' : 'Family'});
+        action.setCallback(this, function(response){
+            var state = response.getState();
+            if(state === 'SUCCESS' && component.isValid()){
+                component.set('v.allStatusMeasure', response.getReturnValue());
+            } else {
+                  var toast = $A.get('e.force:showToast');
+                    	toast.setParams({
+			           'message' : 'the element was not found',
+			           'type' : 'warning',
+			           'mode' : 'dismissible'
+                    	});	
+                    	toast.fire();
+            }
+        });
+        $A.enqueueAction(action);
+	},
+	onChangeStatus : function(component, event, helper) {
+	    	component.find("status").set("v.value", event.getSource().get("v.value")); 
+	},
+	
+	/** @author: David
+	 *  @date: Creation: 31/08/2018
+	 *  @description: method for creating a Measure Progression */
+	createMeasureProgression: function(component, event, helper){
+		var dateProgression = component.find('dateProgression').get('v.value');
+        var pourcentage = component.find('pourcentage').get('v.value');
+        var poucentageProgression = component.find('poucentageProgression').get('v.value');
+        var Description = component.find('Description').get('v.value');
+        var status = component.find('status').get('v.value');
+       
+        	var newMeasureProgression = component.get('v.measureProgression');
+        	newMeasureProgression.orm_dateProgression__c = dateProgression ;
+        	newMeasureProgression.orm_pourcentage__c = pourcentage ;
+        	newMeasureProgression.orm_poucentageProgression__c = poucentageProgression ;
+        	newMeasureProgression.Description = Description ;
+        	newMeasureProgression.Family = status ;
+        	newMeasureProgression.orm_measures__c = component.get("v.idMeasure");
+        	
+        	var action = component.get('c.addMeasureProgression');
+            action.setParams({
+                "item": newMeasureProgression
+            });
+            action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state == "SUCCESS") {
+                component.set("v.measureProgression", response.getReturnValue());
+                var toastEvent = $A.get('e.force:showToast');
+                toastEvent.setParams({ 
+                    'message': $A.get('$Label.c.orm_success_created'),
+                    'type': 'success',
+                    'mode': 'dismissible'
+                });
+                toastEvent.fire();
+                var evt = $A.get("e.c:OrmMeasureProgressionCreatedEvt");
+                   evt.fire();
+                component.set("v.isOpen", false);
+                
+            } else {
+                alert($A.get("$Label.c.orm_error"));
+            }
+        });
+        $A.enqueueAction(action);
+	},
+})

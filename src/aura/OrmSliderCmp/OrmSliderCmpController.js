3367f5b8cac7a88662b3f72316b1a72bbbfc7799
Dibocor <dibocorng@gmail.com>
2018-09-01 20:14:01 +0200

    implementation des slider nouislider

--- src/aura/OrmSliderCmp/OrmSliderCmpController.js
+++ src/aura/OrmSliderCmp/OrmSliderCmpController.js
@@ -1,37 +1,24 @@
 ({
-	jsLoaded: function(component, event, helper) {
-        var min = parseInt(component.get("v.min"), 10);
-        var max = parseInt(component.get("v.max"), 10);
-        var step = parseInt(component.get("v.step"), 10);
-        
+    jsLoaded : function(component, event, helper) {
+        //start first slider
         var slider = component.find('slider').getElement();
-        noUiSlider.create(slider, {
-            start: [min, max],
-            connect: true,
-            tooltips: true,
-            step: step,
-            format: {
-                to: function (value ) {
-					return Math.round(value) + '%';
-                },
-                from: function ( value ) {
-                    return value;
-                }
-            },
-            range: {
-                'min': min,
-                'max': max
-            }
-        });
+        slider = helper.createSlider(component, event, helper, slider);
         
 		slider.noUiSlider.on('change', $A.getCallback(function(range) {
-            var myEvent = $A.get("e.c:OrmReturnRangeChangeEvt");
-        	myEvent.setParams({
-                "filterName": component.get("v.filterName"),
-            	"minValue": range[0].replace('%', ''),
-            	"maxValue": range[1].replace('%', '')
-            });
-			myEvent.fire();
+		    component.set("v.valCostmin", range[0].replace('%', ''))
+		    component.set("v.valCostmax", range[1].replace('%', ''))
         }));
-    }
+        //end first slider
+        
+        //start second slider
+        var slider2 = component.find('slider2').getElement();
+        slider2 = helper.createSlider(component, event, helper, slider2);
+        
+        slider2.noUiSlider.on('change', $A.getCallback(function(range) {
+		    component.set("v.valpourcentagemin", range[0].replace('%', ''))
+		    component.set("v.valpourcentagemax", range[1].replace('%', ''))
+        }));
+        //end first slider
+    },
+   
 })
--- src/aura/OrmSliderCmp/OrmSliderCmpHelper.js
+++ src/aura/OrmSliderCmp/OrmSliderCmpHelper.js
@@ -1,5 +1,27 @@
 ({
-	helperMethod : function() {
-		
+	createSlider : function(component, event, helper, slider) {
+	     var min = parseInt(component.get("v.min"), 10);
+         var max = parseInt(component.get("v.max"), 10);
+         var step = parseInt(component.get("v.step"), 10);
+	
+		 noUiSlider.create(slider, {
+            start: [min, max],
+            connect: true,
+            tooltips: true,
+            step: step,
+            format: {
+                to: function (value ) {
+					return Math.round(value) + '%';
+                },
+                from: function ( value ) {
+                    return value;
+                }
+            },
+            range: {
+                'min': min,
+                'max': max
+            }
+        });
+        return slider;
 	}
 })

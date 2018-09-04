4ed187cc1adb63a10e5ce141c998bc15a5778417
Dibocor <dibocorng@gmail.com>
2018-09-04 19:23:34 +0200

    slider

--- src/aura/OrmSliderCmp/OrmSliderCmpController.js
+++ src/aura/OrmSliderCmp/OrmSliderCmpController.js
@@ -1,8 +1,12 @@
 ({
     jsLoaded : function(component, event, helper) {
+        helper.jsLoaded(component, event, helper);
+    },
+    
+    /*jsLoaded : function(component, event, helper) {
         //start first slider
         var slider = component.find('slider').getElement();
-        slider = helper.createSlider(component, event, helper, slider);
+        slider =  helper.createSlider(component, event, helper, slider);
         
 		slider.noUiSlider.on('change', $A.getCallback(function(range) {
 		    component.set("v.valCostmin", range[0].replace('%', ''))
@@ -12,13 +16,56 @@
         
         //start second slider
         var slider2 = component.find('slider2').getElement();
-        slider2 = helper.createSlider(component, event, helper, slider2);
+        slider2 =  helper.createSlider(component, event, helper, slider2);
         
         slider2.noUiSlider.on('change', $A.getCallback(function(range) {
 		    component.set("v.valpourcentagemin", range[0].replace('%', ''))
 		    component.set("v.valpourcentagemax", range[1].replace('%', ''))
         }));
         //end first slider
+    },*/
+    
+    nextPage : function(component, event, helper) {
+       var showProbilityRanking = component.get("v.showProbilityRanking");
+       var showHsseImpactsRanking = component.get("v.showHsseImpactsRanking");
+       var showBusinessImpactsRanking = component.get("v.showBusinessImpactsRanking");
+       
+       if(showProbilityRanking == true){
+           //helper.jsLoaded(component, event, helper);
+	       component.set("v.showProbilityRanking", false);
+           component.set("v.showHsseImpactsRanking", true);
+       } 
+       
+       if(showHsseImpactsRanking == true){
+           //helper.jsLoaded(component, event, helper);
+	       component.set("v.showHsseImpactsRanking", false);
+           component.set("v.showBusinessImpactsRanking", true);
+       }
+      
     },
+    
+    PrevPage : function(component, event, helper) {
+       var showProbilityRanking = component.get("v.showProbilityRanking");
+       var showHsseImpactsRanking = component.get("v.showHsseImpactsRanking");
+       var showBusinessImpactsRanking = component.get("v.showBusinessImpactsRanking");
+       
+       if(showBusinessImpactsRanking == true){
+           //helper.jsLoaded(component, event, helper);
+	       component.set("v.showHsseImpactsRanking", true);
+           component.set("v.showBusinessImpactsRanking", false);
+       } 
+       
+       if(showHsseImpactsRanking == true){
+           component.set("v.showProbilityRanking", true);
+	       component.set("v.showHsseImpactsRanking", false);
+           
+           helper.jsLoaded(component, event, helper);
+       }
+        
+       
+       
+       
+       
+    }
    
 })
--- src/aura/OrmSliderCmp/OrmSliderCmpHelper.js
+++ src/aura/OrmSliderCmp/OrmSliderCmpHelper.js
@@ -23,5 +23,27 @@
             }
         });
         return slider;
-	}
+	},
+	
+	jsLoaded : function(component, event, helper) {
+        //start first slider
+        var slider = component.find('slider').getElement();
+        slider = this.createSlider(component, event, helper, slider);
+        
+		slider.noUiSlider.on('change', $A.getCallback(function(range) {
+		    component.set("v.valCostmin", range[0].replace('%', ''))
+		    component.set("v.valCostmax", range[1].replace('%', ''))
+        }));
+        //end first slider
+        
+        //start second slider
+        var slider2 = component.find('slider2').getElement();
+        slider2 = this.createSlider(component, event, helper, slider2);
+        
+        slider2.noUiSlider.on('change', $A.getCallback(function(range) {
+		    component.set("v.valpourcentagemin", range[0].replace('%', ''))
+		    component.set("v.valpourcentagemax", range[1].replace('%', ''))
+        }));
+        //end first slider
+    },
 })

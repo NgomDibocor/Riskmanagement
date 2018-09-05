ede245d10527936d449260895c6a89f86f7dfccd
Dibocor <dibocorng@gmail.com>
2018-09-05 14:11:22 +0200

    delete component not use

--- src/aura/OrmAssessmentLayoutCmp/OrmAssessmentLayoutCmpHelper.js
+++ src/aura/OrmAssessmentLayoutCmp/OrmAssessmentLayoutCmpHelper.js
@@ -353,6 +353,44 @@
         component.set("v.showChevronleft", true);   
         component.set("v.showChevronright", true);
     },
+    
+    activeContextActivityShow : function(component, event, helper) {
+        component.set("v.closeFieldDescription",true);
+        var tab1 = component.find('contextId');
+        var tab2 = component.find('riskIdentifId');
+        var tab3 = component.find('riskAnalyseId');
+        var tab4 = component.find('riskTreatmentId');
+        var tab5 = component.find('actionPlanId');
+        
+        //show and Active fruits tab
+        $A.util.removeClass(tab1, 'slds-is-active');
+        $A.util.addClass(tab1, 'slds-is-current');
+        // Hide and deactivate others tab
+        $A.util.removeClass(tab2, 'slds-is-current');
+        $A.util.addClass(tab2, 'slds-is-active');
+ 
+        $A.util.removeClass(tab3, 'slds-is-current');
+        $A.util.addClass(tab3, 'slds-is-active');
+        
+        $A.util.removeClass(tab4, 'slds-is-current');
+        $A.util.addClass(tab4, 'slds-is-active');
+        $A.util.removeClass(tab5, 'slds-is-current');
+        $A.util.addClass(tab5, 'slds-is-active');
+        
+        component.set("v.showContext", false);
+        component.set("v.showContext2", false);
+        component.set("v.showContextActivity", false);
+        component.set("v.showContextActivityShow", true);
+        component.set("v.showContextWorkshop", false);
+        component.set("v.showRiskIdentif", false);
+        component.set("v.showRiskAnalyse", false);
+        component.set("v.showListCauseAndImpact", false);
+        component.set("v.showListMeasure", false);
+        component.set("v.showRiskTreatment", false);
+        component.set("v.showActionPlan", false);
+        component.set("v.showChevronleft", true);   
+        component.set("v.showChevronright", true);
+    },
     activeContextWorkshop : function(component, event, helper) {
         component.set("v.closeFieldDescription",true);
         var tab1 = component.find('contextId');

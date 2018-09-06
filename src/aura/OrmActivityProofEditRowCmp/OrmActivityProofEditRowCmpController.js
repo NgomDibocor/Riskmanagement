2481caced8d4366385cb86f01db4a458b15cb34b
David Diop <diopdavid94@gmail.com>
2018-09-06 13:12:39 +0000

    ActivityProof

--- src/aura/OrmActivityProofEditRowCmp/OrmActivityProofEditRowCmpController.js
+++ src/aura/OrmActivityProofEditRowCmp/OrmActivityProofEditRowCmpController.js
@@ -62,4 +62,64 @@
     {
     	component.set("v.openfilesList",false);
     },
+    /**
+	 * 
+	 * @author David diop
+	 * @version 1.0
+	 * @description method for count the selected checkboxes
+	 * @history 2018-09-05 : David diop - Implementation
+	 */
+	checkboxSelect : function(component, event, helper) {
+		// get the selected checkbox value
+		var selectedRec = event.getSource().get("v.value");
+		// get the selectedCount attrbute value(default is 0) for add/less
+		// numbers.
+		var getSelectedNumber = component.get("v.selectedRowsCount");
+		// check, if selected checkbox value is true then increment
+		// getSelectedNumber with 1
+		// else Decrement the getSelectedNumber with 1
+		if (selectedRec == true) {
+			getSelectedNumber++;
+		} else {
+			getSelectedNumber--;
+		}
+		// set the actual value on selectedCount attribute to show on header
+		// part.
+		component.set("v.selectedRowsCount", getSelectedNumber);
+	},
+	/**
+	 * 
+	 * @author David diop
+	 * @version 1.0
+	 * @description method for delete selected records activity Proof
+	 * @history 2018-09-05 : David diop - Implementation
+	 */
+	deleteSelected : function(component, event, helper) {
+
+		// create var for store record id's for selected checkboxes
+		var delId = [];
+		// get all checkboxes
+		var getAllId = component.find("boxPack");
+		// If the local ID is unique[in single record case], find() returns the
+		// component. not array
+		if (!Array.isArray(getAllId)) {
+			if (getAllId.get("v.value") == true) {
+				delId.push(getAllId.get("v.text"));
+			}
+		} else {
+			// play a for loop and check every checkbox values
+			// if value is checked(true) then add those Id (store in Text
+			// attribute on checkbox) in delId var.
+			for (var i = 0; i < getAllId.length; i++) {
+				if (getAllId[i].get("v.value") == true) {
+					delId.push(getAllId[i].get("v.text"));
+				}
+			}
+		}
+
+		// call the helper function and pass all selected record id's.
+		helper.deleteSelectedHelper(component, event, delId);
+
+	},
+    
 })

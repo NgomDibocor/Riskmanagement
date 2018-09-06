3b0b6a731d10347b921e7b0949a80c22ffc11d03
David Diop <diopdavid94@gmail.com>
2018-09-06 09:44:13 +0000

    text

--- src/aura/OrmActivityProofListCmp/OrmActivityProofListCmpController.js
+++ src/aura/OrmActivityProofListCmp/OrmActivityProofListCmpController.js
@@ -3,6 +3,7 @@
 		component.set("v.idActivity", event.getParam('idActivity'));
 		var idActivity = component.get('v.idActivity');
 		console.log(idActivity);
+		helper.getAllActivityProofByActivity(component, event);
 	},
 	
 	openNewActivityProof : function(component, event, helper) {
@@ -12,6 +13,44 @@
 	            "idActivity": idActivity
 	        });
         evt.fire();
-		
 	},
+	/**
+     * CreatedBy @David Diop
+     * @version 1.0
+	 * @description method cancel save MeasureProgression
+	 * @history 
+	 * 2018-08-31 : David diop - Implementation
+     */
+	cancel : function(component,event,helper) {
+       // on cancel refresh the view (This event is handled by the one.app container. It’s supported in Lightning Experience, the Salesforce app, and Lightning communities. ) 
+       component.set("v.showSaveCancelBtn",false);
+    },
+    save: function(component, event, helper) {
+		if (helper.requiredValidation(component, event)){
+              // call the saveAccount apex method for update inline edit fields update 
+               var action = component.get("c.updateActiviteProof");
+               action.setParams({
+            	   'activityProof': component.get("v.activityProof")
+               });
+                  
+	           action.setCallback(this, function(response) {
+	               var state = response.getState();
+	               if (state === "SUCCESS") {
+	                    var measuresProgressions = response.getReturnValue();
+	                    // set cause list with return value from server.
+	                    component.set("v.activityProof", measuresProgressions);
+	                    // Hide the save and cancel buttons by setting the 'showSaveCancelBtn' false 
+	                    component.set("v.showSaveCancelBtn", false);
+	                    var toast = $A.get('e.force:showToast');
+			            toast.setParams({
+			            	'message' : $A.get('$Label.c.orm_updated'),
+			                'type' : 'success',
+			                'mode' : 'dismissible'
+			            });	
+			            toast.fire();
+			       }
+	           });
+	           $A.enqueueAction(action);
+        } 
+        },
 })
--- src/aura/OrmActivityProofListCmp/OrmActivityProofListCmpHelper.js
+++ src/aura/OrmActivityProofListCmp/OrmActivityProofListCmpHelper.js
@@ -1,5 +1,27 @@
 ({
-	helperMethod : function() {
+	
+	getAllActivityProofByActivity : function(component, event) {	
 		
-	}
+        var action = component.get("c.getAllMeasuresProgressionByMeasure");
+        action.setParam('idActivity', component.get("v.idActivity"));
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+            	//console.log('measure '+ JSON.stringify(response.getReturnValue()));
+            	component.set("v.activityProof ", response.getReturnValue());
+            	component.set("v.activityProofTemp ", response.getReturnValue());
+            }else{
+            	alert($A.get('$Label.c.orm_not_found'));
+            }
+        });
+        $A.enqueueAction(action);
+	},
+	requiredValidation : function(component,event) {
+        // get all causes.. 	
+        var allRecords = component.get("v.activityProof");
+        
+        var isValid = true;
+        
+        return isValid;
+    },
 })

a29b67ae6afa60c96c7f4dfbc2985a6b964bdc98
David Diop <diopdavid94@gmail.com>
2018-09-27 14:54:47 +0000

    test selected riskCategory

--- src/aura/OrmRiskIdentificationLayoutCmp/OrmRiskIdentificationLayoutCmpController.js
+++ src/aura/OrmRiskIdentificationLayoutCmp/OrmRiskIdentificationLayoutCmpController.js
@@ -51,6 +51,27 @@
                 title:  $A.get("$Label.c.orm_edit_button_title") 
             },
         }]);
+        var action = component.get('c.getSelectOptions');
+        action.setParams({
+            'objObject': component.get("v.risk"),
+            'fld': 'orm_categorieRisk__c'
+        });
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === 'SUCCESS' && component.isValid()) {
+                component.set('v.allCategorieRisk', response.getReturnValue());
+                console.log(response.getReturnValue()[0]);
+                component.set('v.categorieRisk', response.getReturnValue()[0]);
+                var evtSpinner = $A.get("e.c:OrmHideSpinnerEvt");
+                evtSpinner.fire();
+                
+            } else {
+
+                alert($A.get('$Label.c.orm_not_found'));
+            }
+        });
+
+        $A.enqueueAction(action);
         helper.fetchPicklist(component, event);
     },
     /*    

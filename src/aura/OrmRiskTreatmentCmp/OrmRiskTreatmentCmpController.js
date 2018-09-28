b75dfb0e428e05b79d028d550b1bc220de512633
David Diop <diopdavid94@gmail.com>
2018-09-28 14:07:19 +0000

    add unit and cost/effect to measure

--- src/aura/OrmRiskTreatmentCmp/OrmRiskTreatmentCmpController.js
+++ src/aura/OrmRiskTreatmentCmp/OrmRiskTreatmentCmpController.js
@@ -1,24 +1,24 @@
 ({
-/**
- *
- * @author David diop
- * @version 1.0
- * @description method doInit
- * @history 
- * 2018-08-24 : David diop - Implementation
- */
+    /**
+     *
+     * @author David diop
+     * @version 1.0
+     * @description method doInit
+     * @history 
+     * 2018-08-24 : David diop - Implementation
+     */
     measureShow: function(component, event, helper) {
         helper.fetchPicklist(component, event, event.getParam('MeasureId'));
-        
+
     },
-/**
- *
- * @author David diop
- * @version 1.0
- * @description method Update measure
- * @history 
- * 2018-08-24 : David diop - Implementation
- */
+    /**
+     *
+     * @author David diop
+     * @version 1.0
+     * @description method Update measure
+     * @history 
+     * 2018-08-24 : David diop - Implementation
+     */
     updateMeasure: function(component, event, helper) {
         var idAssessmentRisk = component.get("v.idAssessmentRisk");
         var measureData = component.get("v.measureData");
@@ -35,15 +35,18 @@
 
         var measureResponsable = component.find("measureResponsable");
         measureData.orm_measureResponsable__c = measureResponsable.get("v.value");
-        
+
         var cost = component.find("costEffect");
         measureData.orm_Cost_Effect__c = cost.get("v.value");
 
         var justification = component.find("justification");
         measureData.orm_justification_Cost_Effect__c = justification.get("v.value");
-        
+
         var description = component.find("description");
         measureData.orm_description__c = description.get("v.value");
+
+        var unit = component.find("unit");
+        measureData.orm_organisation__c = unit.get("v.value");
 
         var action = component.get('c.add');
         action.setParams({
@@ -54,28 +57,28 @@
             if (state == "SUCCESS") {
                 component.set("v.measureData", response.getReturnValue());
                 var toastEvent = $A.get('e.force:showToast');
-                toastEvent.setParams({ 
+                toastEvent.setParams({
                     'message': $A.get('$Label.c.orm_success_created'),
                     'type': 'success',
                     'mode': 'dismissible'
                 });
                 toastEvent.fire();
                 component.set("v.displaySaveCancelBtn", false);
-                
+
             } else {
                 alert($A.get("$Label.c.orm_error"));
             }
         });
         $A.enqueueAction(action);
     },
- /**
- *
- * @author David diop
- * @version 1.0
- * @description method description fields startDate
- * @history 
- * 2018-08-27 : David diop - Implementation
- */
+    /**
+     *
+     * @author David diop
+     * @version 1.0
+     * @description method description fields startDate
+     * @history 
+     * 2018-08-27 : David diop - Implementation
+     */
     onStartDate: function(component, event, helper) {
         var evt = $A.get("e.c:OrmSendValuesFieldDescriptionEvt");
         evt.setParams({
@@ -85,14 +88,14 @@
         evt.fire();
         component.set("v.displaySaveCancelBtn", true);
     },
- /**
- *
- * @author David diop
- * @version 1.0
- * @description method description fields endDate
- * @history 
- * 2018-08-27 : David diop - Implementation
- */
+    /**
+     *
+     * @author David diop
+     * @version 1.0
+     * @description method description fields endDate
+     * @history 
+     * 2018-08-27 : David diop - Implementation
+     */
     onEndDate: function(component, event, helper) {
         var evt = $A.get("e.c:OrmSendValuesFieldDescriptionEvt");
         evt.setParams({
@@ -102,14 +105,14 @@
         evt.fire();
         component.set("v.displaySaveCancelBtn", true);
     },
- /**
- *
- * @author David diop
- * @version 1.0
- * @description method description fields status
- * @history 
- * 2018-08-27 : David diop - Implementation
- */
+    /**
+     *
+     * @author David diop
+     * @version 1.0
+     * @description method description fields status
+     * @history 
+     * 2018-08-27 : David diop - Implementation
+     */
     onChangeStatusMeasure: function(component, event, helper) {
         component.find("statusMeasure").set("v.value", event.getSource().get("v.value"));
         var evt = $A.get("e.c:OrmSendValuesFieldDescriptionEvt");
@@ -120,16 +123,16 @@
         evt.fire();
         component.set("v.displaySaveCancelBtn", true);
     },
-    
-     /**
- *
- * @author David diop
- * @version 1.0
- * @description method description fields status
- * @history 
- * 2018-08-27 : David diop - Implementation
- */
-    onChangeMeasureResponsable : function(component, event, helper) {
+
+    /**
+     *
+     * @author David diop
+     * @version 1.0
+     * @description method description fields status
+     * @history 
+     * 2018-08-27 : David diop - Implementation
+     */
+    onChangeMeasureResponsable: function(component, event, helper) {
         component.find("measureResponsable").set("v.value", event.getSource().get("v.value"));
         var evt = $A.get("e.c:OrmSendValuesFieldDescriptionEvt");
         evt.setParams({
@@ -139,16 +142,16 @@
         evt.fire();
         component.set("v.displaySaveCancelBtn", true);
     },
- /**
- *
- * @author David diop
- * @version 1.0
- * @description method description fields cost
- * @history 
- * 2018-08-27 : David diop - Implementation
- */
-    onChangecost: function(component, event, helper) {
-      component.find("costEffect").set("v.value", event.getSource().get("v.value"));
+    /**
+     *
+     * @author David diop
+     * @version 1.0
+     * @description method description fields status
+     * @history 
+     * 2018-08-27 : David diop - Implementation
+     */
+    onChangeUnit: function(component, event, helper) {
+        component.find("unit").set("v.value", event.getSource().get("v.value"));
         var evt = $A.get("e.c:OrmSendValuesFieldDescriptionEvt");
         evt.setParams({
             "nomField": $A.get("$Label.c.search_title_label"),
@@ -157,14 +160,32 @@
         evt.fire();
         component.set("v.displaySaveCancelBtn", true);
     },
- /**
- *
- * @author David diop
- * @version 1.0
- * @description method description fields Description
- * @history 
- * 2018-08-27 : David diop - Implementation
- */
+    /**
+     *
+     * @author David diop
+     * @version 1.0
+     * @description method description fields cost
+     * @history 
+     * 2018-08-27 : David diop - Implementation
+     */
+    onChangecost: function(component, event, helper) {
+        component.find("costEffect").set("v.value", event.getSource().get("v.value"));
+        var evt = $A.get("e.c:OrmSendValuesFieldDescriptionEvt");
+        evt.setParams({
+            "nomField": $A.get("$Label.c.search_title_label"),
+            "descriptionField": $A.get("$Label.c.search_description_title")
+        });
+        evt.fire();
+        component.set("v.displaySaveCancelBtn", true);
+    },
+    /**
+     *
+     * @author David diop
+     * @version 1.0
+     * @description method description fields Description
+     * @history 
+     * 2018-08-27 : David diop - Implementation
+     */
     onDescription: function(component, event, helper) {
         var evt = $A.get("e.c:OrmSendValuesFieldDescriptionEvt");
         evt.setParams({
@@ -174,14 +195,14 @@
         evt.fire();
         component.set("v.displaySaveCancelBtn", true);
     },
-  /**
- *
- * @author David diop
- * @version 1.0
- * @description method cancel update measure
- * @history 
- * 2018-08-27 : David diop - Implementation
- */
+    /**
+     *
+     * @author David diop
+     * @version 1.0
+     * @description method cancel update measure
+     * @history 
+     * 2018-08-27 : David diop - Implementation
+     */
     cancel: function(component, event, helper) {
         component.set("v.displaySaveCancelBtn", false);
     },
--- src/aura/OrmRiskTreatmentCmp/OrmRiskTreatmentCmpHelper.js
+++ src/aura/OrmRiskTreatmentCmp/OrmRiskTreatmentCmpHelper.js
@@ -26,40 +26,52 @@
                                     var state = response.getState();
                                     if (state === 'SUCCESS') {
                                         component.set('v.allUser', response.getReturnValue());
-
-                                        // get the measure object
-                                        var actionMeasure = component.get("c.getMeasure");
-                                        actionMeasure.setParams({
-                                            "idMeasure": idMeasure
-                                        });
-                                        actionMeasure.setCallback(this, function(response) {
+                                        
+                                        var actionUnit = component.get("c.getOrganisations");
+                                        actionUnit.setCallback(this, function(response) {
                                             var state = response.getState();
                                             if (state === 'SUCCESS') {
-                                                component.set('v.measureData', response.getReturnValue());
-                                                component.set("v.displaySaveCancelBtn", false);
-                                                //put value in the inputSelect
-                                                component.find("statusMeasure").set("v.value", component.get('v.measureData').orm_measure_Status__c);
-                                                component.find("measureResponsable").set("v.value", component.get('v.measureData').orm_measureResponsable__c);
-                                                component.find("costEffect").set("v.value", component.get('v.measureData').orm_Cost_Effect__c);
-                                                var evtSpinner = $A.get("e.c:OrmHideSpinnerEvt");
-                                                evtSpinner.fire();
+                                                component.set('v.allUnit', response.getReturnValue());
+                                                // get the measure object
+                                                var actionMeasure = component.get("c.getMeasure");
+                                                actionMeasure.setParams({
+                                                    "idMeasure": idMeasure
+                                                });
+                                                actionMeasure.setCallback(this, function(response) {
+                                                    var state = response.getState();
+                                                    if (state === 'SUCCESS') {
+                                                        component.set('v.measureData', response.getReturnValue());
+                                                        component.set("v.displaySaveCancelBtn", false);
+                                                        //put value in the inputSelect
+                                                        component.find("statusMeasure").set("v.value", component.get('v.measureData').orm_measure_Status__c);
+                                                        component.find("measureResponsable").set("v.value", component.get('v.measureData').orm_measureResponsable__c);
+                                                        component.find("costEffect").set("v.value", component.get('v.measureData').orm_Cost_Effect__c);
+                                                        component.find("unit").set("v.value", component.get('v.measureData').orm_organisation__c);
+                                                        var evtSpinner = $A.get("e.c:OrmHideSpinnerEvt");
+                                                        evtSpinner.fire();
 
-                                            } else {
-                                                alert($A.get('$Label.c.orm_not_found'));
+                                                    } else {
+                                                        alert($A.get('$Label.c.orm_not_found'));
+                                                    }
+                                                });
+                                                $A.enqueueAction(actionMeasure);
+                                                
+                                                 } else {
+                                                alert($A.get("$Label.c.orm_not_found"));
                                             }
                                         });
-                                        $A.enqueueAction(actionMeasure);
+                                        $A.enqueueAction(actionUnit);
+                                            } else {
+                                                alert($A.get("$Label.c.orm_not_found"));
+                                            }
+                                        });
+                                        $A.enqueueAction(actionUser);
+
                                     } else {
-                                        alert($A.get("$Label.c.orm_not_found"));
+                                        alert($A.get('$Label.c.orm_not_found'));
                                     }
                                 });
-                                $A.enqueueAction(actionUser);
-                                
-                               } else {
-                                alert($A.get('$Label.c.orm_not_found'));
-                            }
-                        });
-                        $A.enqueueAction(actioncostEffect);
+                                $A.enqueueAction(actioncostEffect);
                             } else {
                                 alert($A.get('$Label.c.orm_not_found'));
                             }

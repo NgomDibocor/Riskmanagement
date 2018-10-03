7324a788ed5e6b643709a1754fc0b6c55b978210
ngomsalimata <salifadou@gmail.com>
2018-10-03 10:00:12 +0000

    show single modal in contactworkshop

--- src/aura/OrmEmailTemplateNewCmp/OrmEmailTemplateNewCmpController.js
+++ src/aura/OrmEmailTemplateNewCmp/OrmEmailTemplateNewCmpController.js
@@ -1,4 +1,15 @@
 ({
 		doInit : function(component, event, helper) {
+        var action = component.get('c.getSelectOptions');    
+        action.setParams({'objObject' : component.get("v.emailTemplate"), 'fld' : 'TemplateType'});
+        action.setCallback(this, function(response){
+            var state = response.getState();
+            if(state === 'SUCCESS' && component.isValid()){
+                component.set('v.allTemplateType', response.getReturnValue());
+            } else {
+                alert("the element was not found");
+            }
+        });
+        $A.enqueueAction(action);        
 	},
 })

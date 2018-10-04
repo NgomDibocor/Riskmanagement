348ebd5d7928407fbeea284e876c4e0f5cda21c3
David Diop <diopdavid94@gmail.com>
2018-10-04 09:13:02 +0000

    test pagination

--- src/aura/OrmRiskIdentificationLayoutCmp/OrmRiskIdentificationLayoutCmpController.js
+++ src/aura/OrmRiskIdentificationLayoutCmp/OrmRiskIdentificationLayoutCmpController.js
@@ -4,7 +4,7 @@
         component.set("v.idAssessment", event.getParam('idAssessment'));
     },
     ormRiskCreatedEvent: function(component, event, helper) {
-    	component.set("v.categorieRisk", event.getParam('riskCategoriy'));
+        component.set("v.categorieRisk", event.getParam('riskCategoriy'));
         helper.fetchPicklist(component, event);
     },
 
@@ -32,13 +32,15 @@
             type: 'text',
             iconName: 'standard:orders'
         }, {
-            label: $A.get("$Label.c.orm_description_risk"), 
+            label: $A.get("$Label.c.orm_description_risk"),
             fieldName: 'RiskDescription',
             type: 'text',
             iconName: 'standard:orders',
-            cellAttributes: { class: "descriptionToolTip" }
+            cellAttributes: {
+                class: "descriptionToolTip"
+            }
         }, {
-            label:  $A.get("$Label.c.orm_risk_category"),
+            label: $A.get("$Label.c.orm_risk_category"),
             fieldName: 'RiskcategorieRisk',
             type: 'text',
             iconName: 'standard:orders'
@@ -46,9 +48,9 @@
             label: $A.get("$Label.c.orm_table_action_label"),
             type: 'button',
             typeAttributes: {
-                label:  $A.get("$Label.c.orm_edit_button_title"),
-                name:   $A.get("$Label.c.orm_edit_button_title"),
-                title:  $A.get("$Label.c.orm_edit_button_title") 
+                label: $A.get("$Label.c.orm_edit_button_title"),
+                name: $A.get("$Label.c.orm_edit_button_title"),
+                title: $A.get("$Label.c.orm_edit_button_title")
             },
         }]);
         var action = component.get('c.getSelectOptions');
@@ -64,7 +66,7 @@
                 component.set('v.categorieRisk', response.getReturnValue()[0]);
                 var evtSpinner = $A.get("e.c:OrmHideSpinnerEvt");
                 evtSpinner.fire();
-                
+
             } else {
 
                 alert($A.get('$Label.c.orm_not_found'));
@@ -107,9 +109,33 @@
                             row.RiskcategorieRisk = row.orm_Risk__r.orm_categorieRisk__c;
                         }
                     }
-                    component.set('v.allRisk', rows);
+                     component.set('v.allRisk', rows);
+                    component.set('v.initialData', response.getReturnValue());
+                    component.set('v.items', response.getReturnValue());
+                    // start pagination
+                    var pageSize = component.get("v.pageSizeBis");
+                    // get size of all the records and then hold into an attribute "totalRecords"
+                    component.set("v.totalRecords", component.get("v.items").length);
+                    // set star as 0
+                    component.set("v.startPage", 0);
+                    var totalRecords = component.get("v.items").length;
+                    //var div = Math.trunc(totalRecords / pageSize);
+                    if (totalRecords === pageSize) {
+                        component.set("v.hideNext", true);
+                        component.set("v.endPage", pageSize - 1);
+                    } else {
+                        component.set("v.hideNext", false);
+                        component.set("v.endPage", pageSize - 1);
+                    }
+                    var PaginationList = [];
+                    for (var i = 0; i < pageSize; i++) {
+                        if (component.get("v.items").length > i)
+                            PaginationList.push(component.get("v.items")[i]);
+                    }
+                    component.set('v.PaginationList', PaginationList);
+                    //end pagination
                     if (rows.length == 0) {
-                    
+
                         var toast = $A.get('e.force:showToast');
                         toast.setParams({
                             'message': $A.get("$Label.c.orm_risk_associated_category") + ' ' + categorieRiskValue,
@@ -151,16 +177,14 @@
             newAssessmentRisk.Id = selectedRow.Id;
             assessmentRisks.push(newAssessmentRisk);
         });
-        if(assessmentRisks.length == 0)
-        {
-        component.set("v.isOpenButton", false);
-      
+        if (assessmentRisks.length == 0) {
+            component.set("v.isOpenButton", false);
+
+        } else {
+            component.set("v.isOpenButton", true);
+            component.set("v.dissociateRisk", assessmentRisks);
         }
-        else{
-        component.set("v.isOpenButton", true);
-        component.set("v.dissociateRisk", assessmentRisks);
-        }
-        
+
     },
 
     /*
@@ -168,35 +192,38 @@
      *
      */
     filter: function(component, event, helper) {
-        var dataRisk = component.get('v.allRiskTemp');
-    
+        //        var dataRisk = component.get('v.allRiskTemp');
+        var dataRisk = component.get('v.initialData');
         var term = component.get('v.filter');
         var regex;
         if ($A.util.isEmpty(term)) {
             helper.fetchPicklist(component, event);
         } else {
             term = "^" + term;
-        try {
-            regex = new RegExp(term, "i");
-        
-            dataRisk = dataRisk.filter(row => regex.test(row.RiskName) || regex.test(row.RiskDescription));
-            	
-        } catch (e) {
-            alert(e);
-        }
-        component.set("v.allRisk",dataRisk);
+            try {
+                regex = new RegExp(term, "i");
+
+                dataRisk = dataRisk.filter(row => regex.test(row.RiskName) || regex.test(row.RiskDescription));
+
+            } catch (e) {
+                alert(e);
+            }
+            //        component.set("v.allRisk",dataRisk);
+            component.set("v.filterPagination", dataRisk);
+            component.set("v.items", component.get("v.filterPagination"));
+            helper.paginationFilterBis(component, event);
         }
     },
     relatedRiskfunction: function(component, event, helper) {
         var relatedassesmentRisk = component.get("v.relatedRisk");
         var riskCategory = component.get("v.categorieRisk");
-        var  categorieRisk = component.find("categorieRiskList").get("v.value");
-        	 if ($A.util.isEmpty(categorieRisk)) {
-                component.find("categorieRisk").set("v.value",riskCategory);
-            } else {
-                component.set("v.categorieRisk", categorieRisk);
-               component.find("categorieRisk").set("v.value",categorieRisk);
-            }
+        var categorieRisk = component.find("categorieRiskList").get("v.value");
+        if ($A.util.isEmpty(categorieRisk)) {
+            component.find("categorieRisk").set("v.value", riskCategory);
+        } else {
+            component.set("v.categorieRisk", categorieRisk);
+            component.find("categorieRisk").set("v.value", categorieRisk);
+        }
         var action = component.get('c.addAssessmentRisks');
         action.setParams({
             "items": relatedassesmentRisk
@@ -206,8 +233,8 @@
             if (component.isValid() && state == "SUCCESS") {
                 //notify that list AssessmentRisk Is Not Empty now
                 var evt = $A.get("e.c:OrmListAssessmentRiskIsNotEmpty");
-	            evt.fire();
-	            
+                evt.fire();
+
                 var toast = $A.get('e.force:showToast');
                 toast.setParams({
                     'message': $A.get("$Label.c.orm_success_associated"),
@@ -240,10 +267,10 @@
         action.setCallback(this, function(response) {
             var state = response.getState();
             if (component.isValid() && state == "SUCCESS") {
-            
+
                 var evtnotify = $A.get("e.c:OrmNotifyAfterdeletingAssessmentRiskEvt");
                 evtnotify.fire();
-                
+
                 var toast = $A.get('e.force:showToast');
                 toast.setParams({
                     'message': $A.get("$Label.c.orm_successful_dissociation"),
@@ -326,9 +353,36 @@
                     }
                     var assessmentRisks = component.get('v.allRisk');
                     assessmentRisks.forEach(function(assessmentRisk) {
-                    rows = rows.filter(row => row.Id !== assessmentRisk.orm_Risk__c);
+                        rows = rows.filter(row => row.Id !== assessmentRisk.orm_Risk__c);
                     });
-                    component.set('v.allRiskList', rows);
+//                    component.set('v.allRiskList', rows);
+                    
+                    component.set('v.initialData', rows);
+                component.set('v.items',rows);
+                   // start pagination
+                    var pageSize = component.get("v.pageSizeBis");
+	                // get size of all the records and then hold into an attribute "totalRecords"
+	                component.set("v.totalRecords", component.get("v.items").length);
+	                // set star as 0
+	                component.set("v.startPage",0);
+	                var totalRecords = component.get("v.items").length;
+				    //var div = Math.trunc(totalRecords / pageSize);
+	                if(totalRecords === pageSize){
+	                  component.set("v.hideNext", true);
+	                  component.set("v.endPage", pageSize - 1);
+	                }else{
+	                  component.set("v.hideNext", false);
+	                  component.set("v.endPage", pageSize - 1);
+	                }
+	                var PaginationList = [];
+	                for(var i=0; i< pageSize; i++){
+	                    if(component.get("v.items").length> i)
+	                        PaginationList.push(component.get("v.items")[i]);    
+	                }
+	                component.set('v.PaginationList', PaginationList);
+                //end pagination
+                    
+                    
                     if (rows.length == 0) {
                         var toast = $A.get('e.force:showToast');
                         toast.setParams({
@@ -351,7 +405,8 @@
      *
      */
     filterByRisk: function(component, event, helper) {
-        var data = component.get("v.allRiskListTemp");
+        //        var data = component.get("v.allRiskListTemp");
+        var data = component.get('v.initialData');
         var term = component.get("v.filterRisk");
         var regex;
         if ($A.util.isEmpty(term)) {
@@ -366,11 +421,16 @@
         } catch (e) {
             alert(e);
         }
-        component.set("v.allRiskList", data);
+        //        component.set("v.allRiskList", data);
+
+        component.set("v.filterPagination", data);
+        component.set("v.items", component.get("v.filterPagination"));
+        helper.paginationFilterBis(component, event);
     },
-    closeModal: function(component) {
+    closeModal: function(component, event, helper) {
         // for Hide/Close Model,set the "isOpen" attribute to "False"
         component.set("v.isOpen", false);
+        helper.fetchPicklist(component, event);
     },
     handleRowAction: function(component, event, helper) {
         var row = event.getParam('row');

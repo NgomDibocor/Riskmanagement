774a1c801b7e6b7e69110f536a6dfc0d8a5bf488
Dibocor <dibocorng@gmail.com>
2018-10-23 19:20:32 +0200

    hide

--- src/aura/causeTextCmp/causeTextCmpController.js
+++ src/aura/causeTextCmp/causeTextCmpController.js
@@ -23,55 +23,57 @@
     },
 
     selectCauses: function(component, event, helper) {
-    	var current = component.get("v.currentPage");
+        var current = component.get("v.currentPage");
         var dTable = component.find("accountTable");
         var selectedRows = dTable.getSelectedRows();
         var pgName = "page" + current;
         component.get("v.SelectedAccount")[pgName] = selectedRows;
-  
-     },
+
+    },
     deleteCausesfunction: function(component, event, helper) {
-       var myMap  = component.get("v.SelectedAccount");
-        var idCauses = [];
-        var lengthMap = Object.keys(myMap).length;
-        	if(lengthMap == 0)
-        	{
-        	
-        		var toast = $A.get('e.force:showToast');
-        		toast.setParams({
+        var dTable = component.find("accountTable");
+        var selectedRows = dTable.getSelectedRows();
+        console.log("selectedRows in delete", selectedRows);
+        if(selectedRows.length == 0){
+            var toast = $A.get('e.force:showToast');
+            toast.setParams({
                 'message': $A.get("$Label.c.orm_warning_checked_checkbox"),
                 'type': 'warning',
                 'mode': 'dismissible'
             });
             toast.fire()
-        	}
-        	else{
-        for(var i=0; i< lengthMap; i++){
-            var page = 'page'+i;
-            for(var j=0;j<myMap[page].length; j++){
-                idCauses.push(myMap[page][j].Id);
-            }
+        }else{
+                var myMap = component.get("v.SelectedAccount");
+		        var idCauses = [];
+		        var lengthMap = Object.keys(myMap).length;
+		        
+	            for (var i = 0; i < lengthMap; i++) {
+	                var page = 'page' + i;
+	                for (var j = 0; j < myMap[page].length; j++) {
+	                    idCauses.push(myMap[page][j].Id);
+	                }
+	            }
+	            console.log("id Cause", idCauses);
+	
+	            //		call apex class method
+	            var action = component.get('c.deleteCauses');
+	            // pass the all selected record's Id's to apex method 
+	            action.setParams({
+	                "causeIds": idCauses
+	            });
+	            action.setCallback(this, function(response) {
+	                //store state of response
+	                var state = response.getState();
+	                if (state === "SUCCESS") {
+	                    //component.set("v.SelectedAccount", []);
+	                    helper.getCauses(component, component.get("v.idAssessmentRisk"));
+	                }
+	            });
+	            $A.enqueueAction(action);
         }
-       console.log("id Cause", idCauses);
-       
-//		call apex class method
-		var action = component.get('c.deleteCauses');
-		// pass the all selected record's Id's to apex method 
-		action.setParams({
-			"causeIds": idCauses
-		});
-		action.setCallback(this, function(response) {
-			//store state of response
-			var state = response.getState();
-			if (state === "SUCCESS") {
-				component.set("v.SelectedAccount",[]);
-				helper.getCauses(component, component.get("v.idAssessmentRisk"));
-			}
-		});
-		$A.enqueueAction(action);
-		}
-	},
-	filter: function(component, event, helper) {
+        
+    },
+    filter: function(component, event, helper) {
 
         //var causesTemp = component.get('v.causesTemp');
         var causesTemp = component.get('v.AccountData');

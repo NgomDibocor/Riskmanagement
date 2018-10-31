f768ba48fa8637f8fa5f1f253b394a968f952af7
David Diop <diopdavid94@gmail.com>
2018-10-31 17:12:37 +0000

    test

---
+++ src/aura/OrmMeasureEditCmp/OrmMeasureEditCmpController.js
@@ -0,0 +1,77 @@
+({
+    openOrmMeasureNewCmp: function(component, event, helper) {
+        component.set("v.isOpen", true);
+        component.set('v.idMeasure', event.getParam('idMeasure'));
+        var actionstatus = component.get("c.getSelectOptions");
+        actionstatus.setParams({
+            'objObject': component.get("v.objInfo"),
+            'fld': 'orm_measureCategorie__c'
+        });
+        actionstatus.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === 'SUCCESS') {
+                component.find("measureCategorie").set("v.value", event.getSource().get("v.value"));
+                component.set('v.measureCategorie', response.getReturnValue());
+                var actionMeasure = component.get("c.getMeasuresByAssessmentRisk");
+                actionMeasure.setParams({
+                    "idMeasure":component.get("v.idMeasure")
+                });
+                actionMeasure.setCallback(this, function(response) {
+                    var state = response.getState();
+                    if (state === 'SUCCESS') {
+                        component.set('v.measureData', response.getReturnValue());
+
+                    } else {
+                        alert($A.get('$Label.c.orm_not_found'));
+                    }
+                });
+                $A.enqueueAction(actionMeasure);
+            } else {
+                alert("l'Element n'a pas été retrouvé");
+            }
+        });
+        $A.enqueueAction(actionstatus);
+    },
+     measureCategorieChange: function(component, event, helper) {
+        component.find("measureCategorie").set("v.value", event.getSource().get("v.value"));
+    },
+     createItem: function(component, event, helper) {
+        var name = component.find('name').get('v.value');
+        var description = component.find('description').get('v.value');
+        var categoryMeasure = component.find('measureCategorie').get('v.value');
+        var measureCategory = component.get("v.measureData").orm_measureCategorie__c;
+        var measure = component.get("v.measureData");
+        if ($A.util.isEmpty(categoryMeasure)) {
+                measure.orm_measureCategorie__c = measureCategory;
+            } else {
+                measure.orm_measureCategorie__c = categoryMeasure;
+            }
+            measure.Name= name;
+            measure.Description = description;
+            measure.orm_assessmentRisk__c = component.get("v.idAssessmentRisk");
+            var actionMeasure = component.get('c.updateMeasure');
+            actionMeasure.setParams({
+                "measure": measure
+            });
+            actionMeasure.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state == "SUCCESS") {
+                component.set("v.measureData", response.getReturnValue());
+                var toastEvent = $A.get('e.force:showToast');
+                toastEvent.setParams({ 
+                    'message': $A.get('$Label.c.orm_success_created'),
+                    'type': 'success',
+                    'mode': 'dismissible'
+                });
+                toastEvent.fire();
+                 var evt = $A.get("e.c:OrmMeasureCreatedEvt");
+                    evt.fire();
+                component.set("v.isOpen", false);
+                
+            } else {
+                alert($A.get("$Label.c.orm_error"));
+            }
+        });
+        $A.enqueueAction(actionMeasure);
+        }
+})
--- src/aura/OrmMeasureListCmp/OrmMeasureListCmpHelper.js
+++ src/aura/OrmMeasureListCmp/OrmMeasureListCmpHelper.js
@@ -1,54 +1,50 @@
 ({	
-	requiredValidation : function(component,event) {
-        // get all causes.. 	
-        var allRecords = component.get("v.measures");
-        var isValid = true;
-        // play a for loop on all account list and check that account name is not null,   
-        for(var i = 0; i < allRecords.length; i++) {
-            if(allRecords[i].orm_description__c == null || allRecords[i].orm_description__c.trim() == ''){
-                alert('Complete this field : Row No ' + (i+1) + ' Description is null' );
-                isValid = false;
-            }  
-        }
-        return isValid;
-    },
-    
 	getAllMeasuresByAssessmentRisk : function(component, event) {	
 		
         var action = component.get("c.getAllMeasuresByAssessmentRisk");
         action.setParam('idAssRisk', component.get("v.idAssessmentRisk"));
         action.setCallback(this, function(response) {
             var state = response.getState();
-            if (state === "SUCCESS") {
-            	//console.log('measure '+ JSON.stringify(response.getReturnValue()));
-            	//component.set("v.measures ", response.getReturnValue());
-            	//component.set("v.measuresTemp ", response.getReturnValue());
-            	component.set('v.initialData', response.getReturnValue());
-                component.set('v.items', response.getReturnValue());
-                   // start pagination
-                    var pageSize = component.get("v.pageSizeBis");
-	                // get size of all the records and then hold into an attribute "totalRecords"
-	                component.set("v.totalRecords", component.get("v.items").length);
-	                // set star as 0
-	                component.set("v.startPage",0);
-	                var totalRecords = component.get("v.items").length;
-				    //var div = Math.trunc(totalRecords / pageSize);
-	                if(totalRecords === pageSize){
-	                  component.set("v.hideNext", true);
-	                  component.set("v.endPage", pageSize - 1);
-	                }else{
-	                  component.set("v.hideNext", false);
-	                  component.set("v.endPage", pageSize - 1);
-	                }
-	                var PaginationList = [];
-	                for(var i=0; i< pageSize; i++){
-	                    if(component.get("v.items").length> i)
-	                        PaginationList.push(component.get("v.items")[i]);    
-	                }
-	                component.set('v.PaginationList', PaginationList);
-                //end pagination
+            if (state === 'SUCCESS' && component.isValid()) {
+                var pageSize = component.get("v.pageSizeInlineEdit");
+                component.set('v.ListData', response.getReturnValue());
+                // get size of all the records and then hold into an attribute "totalRecords"
+                component.set("v.totalRecords", component.get("v.ListData").length);
+                //Set the current Page as 0
+                component.set("v.currentPage", 0);
+                // set star as 0
+                component.set("v.startPage", 0);
+                var totalRecords = component.get("v.ListData").length;
+                if (totalRecords === pageSize) {
+                    component.set("v.hideNext", true);
+                    component.set("v.endPage", pageSize - 1);
+                } else {
+                    component.set("v.hideNext", false);
+                    component.set("v.endPage", pageSize - 1);
+                }
+                var PaginationList = [];
+                for (var i = 0; i < pageSize; i++) {
+                    if (component.get("v.ListData").length > i) {
+                        PaginationList.push(response.getReturnValue()[i]);
+                    }
+                }
+                component.set('v.PaginationList', PaginationList);
+            } 
+             else {
+                alert('ERROR');
             }
         });
         $A.enqueueAction(action);
-	}
+	},
+	checkIfMapContentIsEmpty : function(component, event, myMap) {
+        console.log("checkIfMapContentIsEmpty start")
+        var lengthMap = Object.keys(myMap).length;
+        for (var i = 0; i < lengthMap; i++) {
+            var page = 'page' + i;
+            if(myMap[page].length != 0){
+              component.set("v.isEmptyMap", false);
+              console.log("isEmptyMap", component.get("v.isEmptyMap"));
+            }
+        }
+    },
 })

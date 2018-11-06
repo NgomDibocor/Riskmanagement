967701042ecaa5eff4553b23f009ecc864b5bb86
David Diop <diopdavid94@gmail.com>
2018-11-06 14:52:20 +0000

    test

--- src/aura/OrmMeasureProgressionCmp/OrmMeasureProgressionCmpHelper.js
+++ src/aura/OrmMeasureProgressionCmp/OrmMeasureProgressionCmpHelper.js
@@ -1,40 +1,12 @@
 ({
-	getAllMeasuresProgressionByMeasure : function(component, event) {	
-		
+    getAllMeasuresProgressionByMeasure: function(component, event) {
+
         var action = component.get("c.getAllMeasuresProgressionByMeasure");
         action.setParam('idMeasure', component.get("v.idMeasure"));
         action.setCallback(this, function(response) {
             var state = response.getState();
             if (state === "SUCCESS") {
-            	//console.log('measure '+ JSON.stringify(response.getReturnValue()));
-            	//component.set("v.measureProgression ", response.getReturnValue());
-            	//component.set("v.measureProgressionTemp ", response.getReturnValue());
-//            	component.set('v.initialData', response.getReturnValue());
-//                component.set('v.items', response.getReturnValue());
-//                   // start pagination
-//                    var pageSize = component.get("v.pageSizeBis");
-//	                // get size of all the records and then hold into an attribute "totalRecords"
-//	                component.set("v.totalRecords", component.get("v.items").length);
-//	                // set star as 0
-//	                component.set("v.startPage",0);
-//	                var totalRecords = component.get("v.items").length;
-//				    //var div = Math.trunc(totalRecords / pageSize);
-//	                if(totalRecords === pageSize){
-//	                  component.set("v.hideNext", true);
-//	                  component.set("v.endPage", pageSize - 1);
-//	                }else{
-//	                  component.set("v.hideNext", false);
-//	                  component.set("v.endPage", pageSize - 1);
-//	                }
-//	                var PaginationList = [];
-//	                for(var i=0; i< pageSize; i++){
-//	                    if(component.get("v.items").length> i)
-//	                        PaginationList.push(component.get("v.items")[i]);    
-//	                }
-//	                component.set('v.PaginationList', PaginationList);
-                //end pagination
-                
-                 var pageSize = component.get("v.pageSizeInlineEdit");
+                var pageSize = component.get("v.pageSizeInlineEdit");
                 component.set('v.ListData', response.getReturnValue());
                 // get size of all the records and then hold into an attribute "totalRecords"
                 component.set("v.totalRecords", component.get("v.ListData").length);
@@ -57,28 +29,20 @@
                     }
                 }
                 component.set('v.PaginationList', PaginationList);
-            }else{
-            	alert($A.get('$Label.c.orm_not_found'));
+            } else {
+                alert($A.get('$Label.c.orm_not_found'));
             }
         });
         $A.enqueueAction(action);
-		},
-//	requiredValidation : function(component,event) {
-//        // get all causes.. 	
-//        var allRecords = component.get("v.measureProgression");
-//        
-//        var isValid = true;
-//        
-//        return isValid;
-//    },
-        checkIfMapContentIsEmpty : function(component, event, myMap) {
+    },
+    checkIfMapContentIsEmpty: function(component, event, myMap) {
         console.log("checkIfMapContentIsEmpty start")
         var lengthMap = Object.keys(myMap).length;
         for (var i = 0; i < lengthMap; i++) {
             var page = 'page' + i;
-            if(myMap[page].length != 0){
-              component.set("v.isEmptyMap", false);
-              console.log("isEmptyMap", component.get("v.isEmptyMap"));
+            if (myMap[page].length != 0) {
+                component.set("v.isEmptyMap", false);
+                console.log("isEmptyMap", component.get("v.isEmptyMap"));
             }
         }
     },

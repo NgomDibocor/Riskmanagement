1a5c3c020259e39999da98af81d595b7b5ea1d83
David Diop <diopdavid94@gmail.com>
2018-11-09 17:12:17 +0000

    test

--- src/aura/OrmMeasureProgressionEditCmp/OrmMeasureProgressionEditCmpHelper.js
+++ src/aura/OrmMeasureProgressionEditCmp/OrmMeasureProgressionEditCmpHelper.js
@@ -1,28 +1,28 @@
 ({
-	addProgressMeasure : function(component,event, measureProgressionData) {
-			var action = component.get('c.updateMeasureProgression');
-            action.setParams({
-                "measureProgression": measureProgressionData
-            });
-            action.setCallback(this, function(response) {
+    addProgressMeasure: function(component, event, measureProgressionData) {
+        var action = component.get('c.updateMeasureProgression');
+        action.setParams({
+            "measureProgression": measureProgressionData
+        });
+        action.setCallback(this, function(response) {
             var state = response.getState();
             if (state == "SUCCESS") {
                 component.set("v.measureProgressionData", response.getReturnValue());
                 var toastEvent = $A.get('e.force:showToast');
-                toastEvent.setParams({ 
+                toastEvent.setParams({
                     'message': $A.get('$Label.c.orm_success_created'),
                     'type': 'success',
                     'mode': 'dismissible'
                 });
                 toastEvent.fire();
                 var evt = $A.get("e.c:OrmMeasureProgressionCreatedEvt");
-                   evt.fire();
+                evt.fire();
                 component.set("v.isOpen", false);
-                
+
             } else {
                 alert($A.get("$Label.c.orm_error"));
             }
         });
         $A.enqueueAction(action);
-	}
+    }
 })
